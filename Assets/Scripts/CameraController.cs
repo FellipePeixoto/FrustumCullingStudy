8a53cab028ed5e480d1b88b26fea8978aba0e17c
Douzane <guilherme.douzane@gmail.com>
2019-05-26 21:36:37 -0300

    NoClip Camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSpeed = 1f;
+    public float cameraSpeed = 4f;
+    public GameObject cameraTransform;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+        transform.eulerAngles -= new Vector3(Input.GetAxis("Mouse Y") * mouseSpeed, -Input.GetAxis("Mouse X") * mouseSpeed, 0.0f);
+
+
+        if (Input.GetKey(KeyCode.W)){
+            transform.position += cameraTransform.transform.forward * (Time.deltaTime * cameraSpeed);
+        }
+
+
+        if (Input.GetKey(KeyCode.S)) {
+            transform.position += Vector3.back * (Time.deltaTime * cameraSpeed);
+        }
+
+
+        if (Input.GetKey(KeyCode.A)) {
+
+            transform.position += Vector3.left * (Time.deltaTime * cameraSpeed);
+        }
+
+
+        if (Input.GetKey(KeyCode.D)) {
+
+            transform.position += Vector3.right * (Time.deltaTime * cameraSpeed);
+        }
+
+    }
+}
